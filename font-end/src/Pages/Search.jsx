420d7e3b824e196476ff43195225fc6e6564a626
Faryn-03 <anhphuocdang@gmail.com>
2024-04-25 15:49:54 +0700

    Search

--- font-end/src/Pages/Search.jsx
+++ font-end/src/Pages/Search.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
+import "./CSS/Search.css";
+import data from "../Components/Assets/all_product.js";
 
-const Search = () => {
+function Search() {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
-    <div>
-      
-    </div>
+    <>
+    <div className="templateContainer">
+      <div className="searchInput_Container">
+        <input type="text" id="searchInput" placeholder="Search here" onChange={(event) => {
+          setSearchTerm(event.target.value);
+        }} />
+      </div>
+      <div className='template_Container'>
+        {
+          data
+            .filter((val) => {
+              if (searchTerm == "") {
+                // return val;
+              } else if (val.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+                return val;
+              }
+            })
+            .map((val) => {
+              return (
+                <div className='template' key={val.id}>
+                  <img src={val.image} alt="" />
+                  <h3>{val.name}</h3>
+                  <p className='price'>{val.new_price} VNĐ</p>
+                </div>
+              )
+            })
+        }
+      </div>
+      </div>
+    </>
   )
 }
 
