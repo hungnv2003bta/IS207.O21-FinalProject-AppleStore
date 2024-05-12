037ff1628c81a1a397faf6b8833918ee2c0a80ce
BTranBui <buitran0205@gmail.com>
2024-05-12 16:24:14 +0700

    up

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
-
 import Navbar from "../../components/navbar/navbar";
+import sidebar from "../../components/sidebar/sidebar";
 import "./home.css";
 
 const Home = () => {
   return (
     <div className="home">
+      <Sidebar />
           <div className="homeContainer">
               <Navbar />
       </div>
