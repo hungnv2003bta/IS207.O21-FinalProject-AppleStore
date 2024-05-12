3c7f170dcefd035f6d778870a5e1cc49e4a8d9bc
BTranBui <buitran0205@gmail.com>
2024-05-12 17:08:16 +0700

    Update Home.jsx

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -1,14 +1,12 @@
 import Navbar from "../../components/navbar/navbar";
-import sidebar from "../../components/sidebar/sidebar";
 import "./home.css";
 
 const Home = () => {
   return (
     <div className="home">
-      <Sidebar />
           <div className="homeContainer">
               <Navbar />
-      </div>
+          </div>
     </div>
   );
 };
