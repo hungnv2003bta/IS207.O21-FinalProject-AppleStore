ca399b3b19b8f21994f7d25bc347c036d21b820e
thuongthuong <thuongthuong241003@gmail.com>
2024-05-12 14:23:47 +0700

    b

---
+++ admin/src/components/widget/widget.jsx
@@ -0,0 +1,11 @@
+import "./widget.css"
+
+const Widget = () => {
+  return (
+    <div className="widget">
+        <div className="left"> left </div>
+        <div className="right"> right </div>
+    </div>
+  ) }
+
+export default Widget
