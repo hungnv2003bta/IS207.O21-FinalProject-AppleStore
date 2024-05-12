95bd645144985bc70a7ef2b8c638f8e8fc8ba1b1
thuongthuong <thuongthuong241003@gmail.com>
2024-05-12 20:06:41 +0700

    test

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import "./home.css";
 import Widget from "../../components/widget/Widget";
+import Table from "../../components/table/Table";
+import { TableContainer } from "@mui/material";
 
 const Home = () => {
   return (
@@ -14,6 +16,10 @@ const Home = () => {
           <Widget type="order" />
           <Widget type="earning" />
           <Widget type="balance" />
+        </div>
+        <div className="listContainer">
+          <div className="listTitle"> Các giao dịch gần đây nhất</div>
+          <Table />
         </div>
         </div>
       </div>
