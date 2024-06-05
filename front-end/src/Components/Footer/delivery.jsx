31af9a938173a31f2fff05a6287f212f77031912
BTranBui <buitran0205@gmail.com>
2024-06-05 19:05:16 +0700

    update delivery

---
+++ front-end/src/Components/Footer/delivery.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './delivery.css';
+
+const DeliveryPolicy = () => {
+    return (
+        <div className="deliverypolicy">
+          <h4>Chính sách giao hàng 6TAO</h4>
+          <p>*Áp dụng từ: 05/06/2024</p>
+    
+          <h1>1. PHẠM VI ÁP DỤNG</h1>
+          <p>Tất cả các tỉnh thành tại Việt Nam.</p>
+    
+          <h1>2. THỜI GIAN NHẬN HÀNG</h1>
+          <p>6TAO nhận giao nhanh trong ngày với khoảng cách từ cửa hàng đến điểm giao là 20 km. Khoảng cách lớn hơn nhân viên của chúng tôi sẽ tư vấn cách thức giao hàng thuận tiện nhất cho khách hàng. Cụ thể:</p>
+        </div>
+      );
+    };
+
+export default DeliveryPolicy;
